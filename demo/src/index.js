75ec6af3a56b48267b6e252e720bb09284613e28
Jose Miguel Bejarano <yo@josemiguel.org>
2017-03-26 22:59:10 +0200

    🎉 initial commit

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,13 +1,56 @@
 import React from 'react'
 import {render} from 'react-dom'
 
-import Component from '../../src'
+import Currency from '../../src'
 
 let Demo = React.createClass({
   render() {
     return <div>
       <h1>react-cformatter Demo</h1>
-      <Component/>
+      <Currency
+				className="prueba"
+				quantity={45685}
+				currency="EUR"
+				locale="es_ES"
+			/>
+
+			<br/>
+
+      <Currency
+				className="prueba"
+				quantity={45685}
+				currency="EUR"
+				locale="es_ES"
+			/>
+
+			<br/>
+
+			<Currency
+				quantity={45685}
+				currency="GBP"
+			/>
+
+			<br/>
+
+			<Currency
+				quantity={45685}
+				currency="USD"
+			/>
+
+			<br/>
+
+			<Currency
+				quantity={45685}
+				currency="USD"
+			/>
+
+			<br/>
+
+			<Currency
+				quantity={345685}
+				currency="USD"
+				pattern="##,### !"
+			/>
     </div>
   }
 })
